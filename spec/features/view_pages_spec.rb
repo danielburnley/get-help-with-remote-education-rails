c633e761ca4bb85ba578be716b9f31e33c21d837
Daniel Burnley <dan@danops.uk>
2020-10-22 15:15:15 +0100

    Add tests for pages

--- spec/features/view_pages_spec.rb
+++ spec/features/view_pages_spec.rb
@@ -2,8 +2,20 @@ require "rails_helper"
 
 RSpec.feature "View pages", type: :feature do
   scenario "Navigate to home" do
-    visit "/pages/home"
+    visit "/"
 
-    expect(page).to have_text("Lorem")
+    expect(page).to have_text("Get Help with Remote Education")
+  end
+
+  scenario "Navigate to device access" do
+    visit "/device-access"
+
+    expect(page).to have_text("Device Access")
+  end
+
+  scenario "Navigate to statutory obligations" do
+    visit "/statutory-obligations"
+
+    expect(page).to have_text("Statutory Obligations")
   end
 end
